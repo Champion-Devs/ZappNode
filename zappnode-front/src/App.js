a6e7cd436b58c7226b387f0bd0957139ee82192a
Khalid <kabcoder@gmail.com>
2020-07-25 11:50:11 +0100

    update the workspace

--- zappnode-front/src/App.js
+++ zappnode-front/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './tailwind.generated.css';
 
 function App() {
   return (
-    <h1>WELCOME TO ZAPPNODE</h1>
+    <h1 className="text-center">WELCOME TO ZAPPNODE</h1>
   );
 }
 
