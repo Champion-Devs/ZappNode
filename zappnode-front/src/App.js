26eff8ce912d53de689db8b5c95627901fa396d4
Sean Norwood <norwood.sean@gmail.com>
2020-07-25 18:14:47 -0500

    feat: add primereact deps

--- zappnode-front/src/App.js
+++ zappnode-front/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 import './tailwind.generated.css';
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
-import Home from "./Home";
+import Home from './Home';
 
 function App() {
   return (
