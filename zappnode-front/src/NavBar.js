bbd92a50538e5f1f7a1f4a282131ecde7af90ffe
Sultan Bouhassoun <sultan.bouhassoun@montyholding.com>
2020-07-25 17:36:34 +0300

    adding NavBar link

--- zappnode-front/src/NavBar.js
+++ zappnode-front/src/NavBar.js
@@ -1,9 +1,42 @@
 import React from 'react';
-
-function NavBar(){
+import {
+    Link
+} from "react-router-dom"
+function NavBar() {
     return (
         <React.Fragment>
-            NavBar
+            <div className="flex">
+                <div className="flex-1">
+                    <Link to="/">
+                        ZappNode
+                    </Link>
+                </div>
+                <div className="flex-1">
+                    <Link to="/features">
+                        Features
+                    </Link>
+                </div>
+                <div className="flex-1">
+                    <Link to="/usage">
+                        Usage
+                    </Link>
+                </div>
+                <div className="flex-1">
+                    <Link to="/pricing">
+                        Pricing
+                    </Link>
+                </div>
+                <div className="flex-1">
+                    <Link to="/login">
+                        Log In
+                    </Link>
+                </div>
+                <div className="flex-1">
+                    <Link to="/register">
+                        Register
+                    </Link>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
