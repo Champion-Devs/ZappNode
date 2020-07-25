fdec90e9a990491e7057f08062094d709b01e316
Sean Norwood <norwood.sean@gmail.com>
2020-07-25 18:17:45 -0500

    chore: update imports for routes and components

--- zappnode-front/src/Home.js
+++ zappnode-front/src/Home.js
@@ -1,46 +1,37 @@
 import React from 'react';
-import {
-    Switch,
-    Route
-} from "react-router-dom";
-
-import NavBar from "./NavBar";
-import Footer from "./Footer";
-
-import Features from "./Features";
-import Usage from "./Usage";
-import Pricing from "./Pricing";
-import LogIn from "./LogIn";
-import Register from "./Register";
+import { Route, Switch } from 'react-router-dom';
+import { Login, Pricing, Register, Usage, Features } from './routes';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 // these will be the main pages for non signed in users
 function Home() {
-    return (
-        <React.Fragment>
-            <NavBar></NavBar>
-                <Switch>
-                    <Route exact path="/features">
-                        <Features />
-                    </Route>
-                    <Route exact path="/usage">
-                        <Usage />
-                    </Route>
-                    <Route exact path="/pricing">
-                        <Pricing />
-                    </Route>
-                    <Route exact path="/logIn">
-                        <LogIn />
-                    </Route>
-                    <Route exact path="/register">
-                        <Register />
-                    </Route>
-                    <Route path="/">
-                        <Features />
-                    </Route>
-                </Switch>
-            <Footer></Footer>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Switch>
+        <Route exact path="/features">
+          <Features />
+        </Route>
+        <Route exact path="/usage">
+          <Usage />
+        </Route>
+        <Route exact path="/pricing">
+          <Pricing />
+        </Route>
+        <Route exact path="/logIn">
+          <Login />
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Features />
+        </Route>
+      </Switch>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default Home;
