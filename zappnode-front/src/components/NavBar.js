03ccf4373c350bc0b5325c79fc93a80c22d5ce0d
Sean Norwood <norwood.sean@gmail.com>
2020-07-25 18:16:59 -0500

    refactor: move components to /components

---
+++ zappnode-front/src/components/NavBar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Menubar } from 'primereact/menubar';
+import { Button } from 'primereact/button';
+
+function NavBar() {
+  const history = useHistory();
+
+  const handleRoute = (path) => {
+    history.push(`${path}`);
+  };
+
+  const menuItems = [
+    {
+      label: 'ZappNode',
+      icon: 'pi pi-home',
+      command: () => handleRoute('/'),
+    },
+    {
+      label: 'Features',
+      command: () => handleRoute('/features'),
+    },
+    {
+      label: 'Usage',
+      command: () => handleRoute('/usage'),
+    },
+    {
+      label: 'Pricing',
+      command: () => handleRoute('/pricing'),
+    },
+  ];
+
+  return (
+    <React.Fragment>
+      <div className="p-3">
+        <Menubar model={menuItems}>
+          <Button label="Login" />
+          <Button style={{ marginLeft: '4px', backgroundColor: 'green' }} label="Register" />
+        </Menubar>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default NavBar;
