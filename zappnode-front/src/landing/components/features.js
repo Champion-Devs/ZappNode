33f8d8061e5fcfd125046291f448c6ff67e4fb9a
Khalid <kabcoder@gmail.com>
2020-07-26 22:33:22 +0100

    setting up landing page project enviornment

---
+++ zappnode-front/src/landing/components/features.js
@@ -0,0 +1,10 @@
+const { useState, useEffect } = require("react")
+
+import react, {useState, useEffect} from 'react';
+import '.../tailwind.generated.css';
+
+const Features = props => {
+    return;
+}
+
+export default Contact;
---
+++ zappnode-front/src/landing/components/header.js
@@ -0,0 +1,10 @@
+const { useState, useEffect } = require("react")
+
+import react, {useState, useEffect} from 'react';
+import '.../tailwind.generated.css';
+
+const Header = props => {
+    return;
+}
+
+export default Contact;
