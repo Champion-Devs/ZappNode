5e48ae1f1081969716729861d67dd76ab5fc4da1
zeyad-kay <zeyad_khaled@outlook.com>
2020-07-28 17:25:20 +0200

    update user model

--- models/user.js
+++ models/user.js
@@ -3,14 +3,18 @@ const {Monitor} = require('./monitor');
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-    username: { type: String , unique: true },
+    name: String, 
+    email: String,
     password: String,
     googleID: String,
     facebookID: String,
     admin: Boolean,
     superAdmin: Boolean,
-    plan: Boolean,
-    userCount: Number,
+    plan: {
+        type: Boolean,
+        default: false
+    },
+    memberCount: Number,
     monitors: [Monitor.schema]
 });
 
