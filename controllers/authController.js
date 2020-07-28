fbce1ed5867972c84e167cc7824d781ffccee14d
Hakim <ozil.hakim@gmail.com>
2020-07-28 22:02:12 +0100

    local signup implementation

---
+++ controllers/authController.js
@@ -0,0 +1,34 @@
+const User = require("../models/user");
+
+exports.signup = async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      throw "Oups! Something is missing";
+    }
+
+    //check if the user exist
+    if (await User.findOne({ email })) {
+      throw "User already exist";
+    }
+
+    //create new user
+    const user = await User.create({ name, email, password });
+
+    if (!user) throw "Somthing went wrong please try again";
+
+    //pass the user to the next middlewar
+    req.user = user;
+
+    res.status(201).json({
+      status: "success",
+      message: "User created successfully",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
