3ded65c0345aae5ee6e53d097460df933762aa02
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-07-29 11:00:23 -0400

    usercontroller, models tweak

--- routes/api/monitors.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const monitor_controller = require('../controllers/monitor_controller.js');
-
-router.delete('/:id', monitor_controller.delete);
-module.exports = router;
