87e330580eb38a5c4f6a4e6bb375e4fa3d6f3d24
Chun Poon <7432363+heyitschun@users.noreply.github.com>
2020-07-29 12:48:06 +0200

    Update navigation.js
    `register` button jumped on hover, because it was missing scale number.

--- zappnode-front/src/landing/components/header/navigation.js
+++ zappnode-front/src/landing/components/header/navigation.js
@@ -10,7 +10,7 @@ const Navigation = props => {
     <span className="font-semibold text-xl tracking-tight">ZappNode</span>
   </div>
   <div className="block lg:hidden">
-    <button className="flex items-center px-3 py-2 border rounded text-white border-green hover:text-white hover:border-white">
+    <button className="flex items-center px-3 py-2 border rounded text-white border-green-500 hover:text-white hover:border-white">
       <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
     </button>
   </div>
@@ -31,7 +31,7 @@ const Navigation = props => {
     </div>
 
     <div>
-      <a href="#" className="trans text-pri inline-block px-5 py-3 text-base leading-none rounded text-white hover:text-primary hover:bg-transparent border-green btn-primary mt-4 lg:mt-0">Register</a>
+      <a href="#" className="trans text-pri inline-block px-5 py-3 text-base leading-none rounded text-white hover:text-primary hover:bg-transparent border border-green-500 btn-primary mt-4 lg:mt-0">Register</a>
     </div>
   </div>
 </nav>
