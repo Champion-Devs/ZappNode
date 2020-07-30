cf184c3150a29729c71f58f640d2444f4cc2f551
Hakim <ozil.hakim@gmail.com>
2020-07-30 15:56:51 +0100

    update passport.js to use email io usermane

--- config/passport.js
+++ config/passport.js
@@ -7,55 +7,62 @@ const bcrypt = require('bcrypt');
 
 // Google Strategy
 module.exports = function (passport) {
-	passport.use(
-		new GoogleStrategy(
-			{
-				clientID: keys.GOOGLE_CLIENT_ID,
-				clientSecret: keys.GOOGLE_CLIENT_SECRET,
-				callbackURL: '/api/auth/google/callback',
-				proxy: true,
-			},
-			(accessToken, refreshToken, profile, cb) => {
-				console.log(accessToken);
-				console.log(profile);
-			}
-		)
-	);
-	// Local Strategy
-	passport.use(
-		new LocalStrategy((username, password, done) => {
-			//match user
-			userModel.findOne({ username: username }, (err, user) => {
-				if (err) {
-					return done(err);
-				}
-				if (!user) {
-					return done(null, false, { message: 'Incorrect Username' });
-				}
-				if (user) {
-					bcrypt.compare(password, user.password, (err, match) => {
-						//passwords match
-						if (err) throw err;
-						if (match) {
-							return done(null, user);
-						} else {
-							return done(null, false, { message: 'Incorrect Password' });
-						}
-					});
-				}
-			});
-		})
-	);
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.GOOGLE_CLIENT_ID,
+        clientSecret: keys.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/api/auth/google/callback',
+        proxy: true,
+      },
+      (accessToken, refreshToken, profile, cb) => {
+        console.log(accessToken);
+        console.log(profile);
+      },
+    ),
+  );
+  // Local Strategy
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: 'email',
+        passwordField: 'password',
+      },
+      (email, password, done) => {
+        //match user
+        userModel.findOne({ email: email }, (err, user) => {
+          if (err) {
+            console.log(err);
+            return done(err);
+          }
+          if (!user) {
+            console.log('inccorect un');
+            return done(null, false, { message: 'Incorrect Username' });
+          }
+          if (user) {
+            bcrypt.compare(password, user.password, (err, match) => {
+              //passwords match
+              if (err) throw err;
+              if (match) {
+                return done(null, user);
+              } else {
+                return done(null, false, { message: 'Incorrect Password' });
+              }
+            });
+          }
+        });
+      },
+    ),
+  );
+  passport.serializeUser(function (user, done) {
+    done(null, user._id);
+  });
 
-	passport.serializeUser(function(user, done) {
-		done(null, user._id);
-	  });
-	
-	  passport.deserializeUser(function(id, done) {
-		userModel.findById(id, function(err, user) {
-		  done(err, user);
-		});
-	  });
+  passport.deserializeUser(function (id, done) {
+    userModel.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
 };
 
 // Facebook Strategy
