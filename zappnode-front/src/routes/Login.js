34a9b6adcf49124eaa7f55eb26fec936c41791ea
heyitschun <cpoon19@gmail.com>
2020-08-04 15:41:47 +0200

    simple basic form for /login and fixed dashboard footer

--- zappnode-front/src/routes/Login.js
+++ zappnode-front/src/routes/Login.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
-  return <React.Fragment>Login</React.Fragment>;
-}
+  const [credentials, setCredentials] = useState(
+    { username: "", password: "" }
+  );
+
+  return (
+    <React.Fragment>
+      <div className="flex justify-center mt-10">
+        <form className="bg-white max-w-xs w-full border border-gray-400 rounded px-8 pt-6 pb-8 mb-4" action="">
+          <h1 className="font-bold text-xl mb-6">Login</h1>
+          <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+              Username
+            </label>
+            <input class="trans appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-green-500" id="username" type="text" value={credentials.username} />
+          </div>
+          <div class="mb-6">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+              Password
+            </label>
+            <input class="trans appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:border-green-500" id="password" type="password" value={credentials.password}/>
+          </div>
+          <div class="flex items-center justify-between">
+            <button class="trans border border-green bg-green-500 hover:bg-white hover:text-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Login
+            </button>
+            <a class="trans inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-400" href="#">
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Login;
