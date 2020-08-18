bee7255b627ac21990eab37f7a18e44def7332d8
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-08-18 00:07:23 -0400

    implements member monitor read

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,13 +1,31 @@
+const User = require('../models/user');
+
 const monitor = {
   create: async (req, res) => {
     try {
+      res.json({ message: 'not yet implemented' });
     } catch (err) {
       throw err;
     }
   },
 
   read: async (req, res) => {
+    //reads a monitor or the monitor list, takes an optional monitor id
     try {
+      const { user_id, monitor_id } = req.body;
+      //if no specific monitor was requested from the user, send them all
+      if (!monitor_id) {
+        User.find({ _id: user_id }).exec((err, success) => {
+          if (err) throw err;
+          res.json(success.monitors);
+        });
+      } else {
+        //otherwise, send the specific monitor
+        User.find({ _id: user_id, monitors: { _id: monitor_id } }).exec((err, success) => {
+          if (err) throw err;
+          res.json(success);
+        });
+      }
     } catch (err) {
       throw err;
     }
@@ -15,6 +33,7 @@ const monitor = {
 
   update: async (req, res) => {
     try {
+      res.json({ message: 'not yet implemented' });
     } catch (err) {
       throw err;
     }
