fcffe05386eba627756d0d8f7f600690ff436db4
heyitschun <cpoon19@gmail.com>
2020-08-18 20:27:20 +0200

    remove Navbar import from Home component

--- zappnode-front/src/Home.js
+++ zappnode-front/src/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Login, Pricing, Register, Usage, Features, Dashboard } from './routes';
-import NavBar from './components/NavBar';
 import Landing from './landing';
 
 // these will be the main pages for non signed in users
