f6f3ae92da990c9f67121920aee5b035f9fed112
heyitschun <cpoon19@gmail.com>
2020-08-23 09:28:26 +0200

    component structure and route added to Home

---
+++ zappnode-front/src/components/dashboard/user-settings/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Account from './account';
+
+const Main = () => {
+  return (
+    <>
+      {/* component imports */}
+      <Account />
+    </>
+  );
+};
+
+export default Main;
---
+++ zappnode-front/src/components/dashboard/user-settings/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './components/main';
+
+const Settings = () => {
+  return (
+    <>
+      <label content="hello" />
+      <Main />
+    </>
+  )
+}
+
+export default Settings;
