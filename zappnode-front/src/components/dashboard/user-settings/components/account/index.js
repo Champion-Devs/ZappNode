9232d7abc8c7a9bdb6a9fd99815abb9064616c55
heyitschun <cpoon19@gmail.com>
2020-08-24 16:08:35 +0200

    stash

--- zappnode-front/src/components/dashboard/user-settings/components/account/index.js
+++ zappnode-front/src/components/dashboard/user-settings/components/account/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const Account = () => {
+  // Placeholder user object:
+  const user = {
+    name: "user1",
+    tz: "CET",
+
+  };
   return (
     <>
       <div>account</div>
