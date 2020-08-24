a7037e5f2dc411be0dd455e80fdf4ada82e87af5
heyitschun <cpoon19@gmail.com>
2020-08-24 21:34:01 +0200

    email, subscription and password sections of profile

--- zappnode-front/src/components/dashboard/user-settings/components/account/index.js
+++ zappnode-front/src/components/dashboard/user-settings/components/account/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card } from 'primereact/card';
+import { InputText } from 'primereact/inputtext';
 
 const Account = () => {
   // Placeholder user object:
@@ -16,15 +17,15 @@ const Account = () => {
   // 'Upgrade' button. If our highest plan is called something other than
   // 'Business' then this must be changed as well.
   if (user.plan !== "Business") {
-    upgradePlan = <button className="transition duration-500 hover:bg-white hover:text-green-500 border border-green-500 rounded bg-green-500 tracking-wider font-bold uppercase text-sm text-white ml-4 px-2">
+    upgradePlan = <button className="transition duration-500 hover:bg-white hover:text-green-500 border hover:border-green-500 rounded bg-green-500 tracking-wider font-bold uppercase text-sm text-white ml-4 px-2">
       Upgrade
     </button>
   } else upgradePlan = null;
   return (
     <>
       <Card className="m-5">
-        <div className="my-2">
-          <h1 className="font-bold text-lg">
+        <div className="my-4">
+          <h1 className="font-bold text-xl">
             {profileIcon}{user.name}
             <span className="text-base float-right">
               {user.plan}
@@ -33,6 +34,34 @@ const Account = () => {
           </h1>
         </div>
         <hr/>
+        <div className="my-4">
+          <h2 className="text-lg font-bold">Email</h2>
+          Your account is registered to <span className="font-bold">{user.email}</span>
+          <span className="font-bold cursor-pointer float-right text-green-500">change e-mail address</span>
+        </div>
+        <div className="my-4">
+          <h2 className="text-lg font-bold">Subscription</h2>
+          You are currently subscribed to our <span className="font-bold">{user.plan}</span> plan
+          <span className="font-bold cursor-pointer float-right text-green-500">upgrade</span>
+        </div>
+        <div className="my-4">
+          <h2 className="text-lg font-bold">Password change</h2>
+          <div className="p-fluid mt-4">
+            <div className="p-field">
+              <label htmlFor="currentPassword">Current password</label>
+              <InputText id="currentPassword" type="password" />
+            </div>
+            <div className="p-field">
+              <label htmlFor="newPassword">New password</label>
+              <InputText id="newPassword" type="password" />
+            </div>
+            <div className="p-field">
+              <label htmlFor="confirmPassword">Confirm password</label>
+              <InputText id="confirmPassword" type="password" />
+            </div>
+          </div>
+          <button className="font-bold tracking-wider px-2 py-2 bg-green-500 text-white rounded transition duration-500 hover:bg-white hover:border-green-500 hover:text-green-500 border">Save</button>
+        </div>
       </Card>
     </>
   )
