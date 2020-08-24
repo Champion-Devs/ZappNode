486886c2d2a45e73d04939b7ca26d94054be2ccb
heyitschun <cpoon19@gmail.com>
2020-08-24 17:25:25 +0200

    user profile overview header

---
+++ zappnode-front/src/components/dashboard/user-settings/README.md
@@ -0,0 +1,1 @@
+User profile page that doubles as a settings page. Should be used as component in the dashboard behind authentication wall.
--- zappnode-front/src/components/dashboard/user-settings/components/account/index.js
+++ zappnode-front/src/components/dashboard/user-settings/components/account/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import { Card } from 'primereact/card';
 
 const Account = () => {
   // Placeholder user object:
   const user = {
     name: "user1",
-    tz: "CET",
-
+    email: "user1@gmail.com",
+    tz: "CET", // Not currently in User schema
+    plan: "Professional"
   };
+  const profileIcon = <i className="pi pi-user mr-4"></i>
+  let upgradePlan;
+
+  // If the user's current plan is lower than the higher plan, then we show an
+  // 'Upgrade' button. If our highest plan is called something other than
+  // 'Business' then this must be changed as well.
+  if (user.plan !== "Business") {
+    upgradePlan = <button className="transition duration-500 hover:bg-white hover:text-green-500 border border-green-500 rounded bg-green-500 tracking-wider font-bold uppercase text-sm text-white ml-4 px-2">
+      Upgrade
+    </button>
+  } else upgradePlan = null;
   return (
     <>
-      <div>account</div>
+      <Card className="m-5">
+        <div className="my-2">
+          <h1 className="font-bold text-lg">
+            {profileIcon}{user.name}
+            <span className="text-base float-right">
+              {user.plan}
+              {upgradePlan}
+            </span>
+          </h1>
+        </div>
+        <hr/>
+      </Card>
     </>
   )
 };
