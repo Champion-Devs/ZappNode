43adfd819d29ca6834b2cbc7a73e87012c1f12e5
Shawn Matyasovszky <shawnmuzick@gmail.com>
2020-09-27 22:32:04 -0400

    fix: conflict w/ member delete

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,9 +1,15 @@
 const { monitor } = require('./userController');
-monitor.delete = async (req, res) => {
-  try {
-    res.status(400).json({ message: 'Monitor deletion by member not allowed, contact administrator' });
-  } catch (err) {
-    throw err;
-  }
+
+const memberMonitor = {
+  create: monitor.create,
+  read: monitor.read,
+  update: monitor.update,
+  delete: async (req, res) => {
+    try {
+      res.status(400).json({ message: 'Monitor deletion by member not allowed, contact administrator' });
+    } catch (err) {
+      throw err;
+    }
+  },
 };
-module.exports = { monitor };
+module.exports = { memberMonitor };
